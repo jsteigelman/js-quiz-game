63bfb2e7396a7e06406905d45cdc2f621ade893b
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-04 17:11:04 -0500

    added gameover screen

---
+++ Assets/Scripts/Gameover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gameover : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore() {
+        finalScoreText.text = "You scored " + scoreKeeper.GetScore() + " points. Way to go!";
+    }
+
+}

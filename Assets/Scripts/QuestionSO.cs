6be3025a3bf058e0b1b23c77bc753199084523fc
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-03 23:12:51 -0500

    set up answers as for loop

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,8 +8,18 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerindex;
 
     public string GetQuestion() {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerindex;
+    }
+
+    public string GetAnswer(int index) {
+        return answers[index];
     }
 }

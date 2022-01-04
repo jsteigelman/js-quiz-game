3899f5f3c04518bff77b058cc79f03220224c7c0
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-03 22:26:16 -0500

    added question as scriptable object

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question" )]
+
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Question text here";
+
+    public string GetQuestion() {
+        return question;
+    }
+}
